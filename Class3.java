a33d3340787b7ffcfd77ed3422ee43b049314829
geeta gohil <gitgohil@yahoo.co.in>
2023-02-15 22:26:14 +0000

    Class3

---
+++ Class3.java
@@ -0,0 +1,12 @@
+package project3a;
+
+public class Class3 {
+    public static void main(String[] args) {
+        System.out.println("Cost effective");
+        System.out.println("Security");
+        System.out.println("Best quality product");
+        System.out.println("Customer satisfaction");
+
+
+    }
+}
